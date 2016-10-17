f027f1d388286fba15d0b742f77a8295f70bf656
Edwin Mwaniki <edwinmwaniki@Edwins-MBP-2.home>
2016-10-16 23:25:06 -0400

    Updated Student

--- Students.js
+++ Students.js
@@ -1,3 +1,15 @@
-/**
- * Created by edwinmwaniki on 10/16/16.
- */
+class Student extends Account{
+
+    private student_id;
+    private school;
+    private classcode;
+    constructor(firstname,lastname,username,password,student_id,school,classcode){
+        super(firstname,lastname,username,password);
+        this.student_id = student_id;
+        this.classcode = classcode;
+        this.school = school;
+    }
+    get firstName(){
+        super
+    }
+}
